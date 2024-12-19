d8b38ea373253588a866da83cddfc6eb63b9d6e7
t-kikuc <tkikuchi07f@gmail.com>
2024-12-19 09:24:27 +0900

    refactor
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

--- src/main.go
+++ src/main.go
@@ -14,27 +14,16 @@ var rootCmd = &cobra.Command{
 	Long:  ``,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func main() {
-	// Register commands
-	commands := []*cobra.Command{
+	rootCmd.AddCommand(
 		stop.NewStopServiceCommand(),
 		stop.NewStopTaskCommand(),
 		stop.NewStopInstanceCommand(),
 		stop.NewStopAllCommand(),
-		// deletetaskdef.NewCommand(),
-		// deletetasksets.NewCommand(),
-	}
-	for _, c := range commands {
-		rootCmd.AddCommand(c)
-	}
+	)
 
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
-}
-
-func init() {
 }
