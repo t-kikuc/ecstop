69adf6f401627ccb5752b67baf335e1f928becd2
t-kikuc <tkikuchi07f@gmail.com>
2024-12-19 21:28:27 +0900

    add test: NewStopTaskCommand flags
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

--- src/stop/task_test.go
+++ src/stop/task_test.go
@@ -7,6 +7,54 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNewStopTaskCommand(t *testing.T) {
+	t.Parallel()
+
+	testcases := []struct {
+		title   string
+		flags   map[string]string
+		wantErr bool
+	}{
+		{
+			title: "valid flags",
+			flags: map[string]string{
+				"cluster": "xxx-cluster",
+				"group":   "xxx-group",
+			},
+			wantErr: false,
+		},
+		{
+			title:   "no flag causes error of OneRequired",
+			flags:   map[string]string{},
+			wantErr: true,
+		},
+		{
+			title: "all flags causes error of MutuallyExclusive",
+			flags: map[string]string{
+				"group":        "xxx-group",
+				"group-prefix": "xxx-group-prefix",
+				"standalone":   "true",
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tc := range testcases {
+		tc := tc
+		t.Run(tc.title, func(t *testing.T) {
+			t.Parallel()
+			cmd := NewStopTaskCommand()
+			for k, v := range tc.flags {
+				cmd.Flags().Set(k, v)
+			}
+			err1 := cmd.ValidateRequiredFlags()
+			err2 := cmd.ValidateFlagGroups()
+
+			assert.Equal(t, tc.wantErr, err1 != nil || err2 != nil)
+		})
+	}
+}
+
 func TestFilterByGroup_match(t *testing.T) {
 	t.Parallel()
 
@@ -43,8 +91,8 @@ func TestFilterByGroup_prefix(t *testing.T) {
 		{"group", "group", true},
 		{"group", "group1", true},
 		{"group", "Group", false},
-		{"group", "", false},
 		{"group", "_group", false},
+		{"group", "", false},
 	}
 
 	for _, tc := range testcases {
